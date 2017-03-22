6cb7366d669c5af1cc9ddee5962f4f87b9ad5b73
Monserrat <monserratdiaz12@gmail.com>
2017-03-22 12:34:22 +0000

    made parameters

--- script.js
+++ script.js
@@ -1,15 +1,85 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
+function square(sides){
+    forward(sides);
+    right(90);
+    forward(sides);
+    right(90);
+    forward(sides);
+    right(90);
+    forward(sides);
+    right(90);
+    forward(sides);
+}
+function triangle(side){
+    right(30);
+    forward(side);
+    right(120);
+    forward(side);
+    right(120);
+    forward(side);
+}
+function rectangle(side1, side2){
+    forward(side1);
+    right(90);
+    forward(side2);
+    right(90);
+    forward(side1);
+    right(90);
+    forward(side2);
+}
+function house(len){
+    square(len);
+    triangle(len);
+}
+function pentagon(size){
+    forward(size);
+    right(72);
+    forward(size);
+    right(72);
+    forward(size);
+    right(72);
+    forward(size);
+    right(72);
+    forward(size);
+}
+function hexagon(size){
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);
+    right(60);
+    forward(size);  
+}
+function dodecagon(size){
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);
+    right(30);
+    forward(size);  
 }
 
 // Type your function call below
